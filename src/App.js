acca9d84c95831139354ef069c974904ad1c4071
Kateryna29 <rinaniky@gmail.com>
2022-12-29 16:06:47 +0100

    Page App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="container">
+        <HeadCity />
+        <SearchInformation />
+      </div>
+      <div className="About-creator">
+        <a href="https://github.com/Kateryna29/my-react-app" target="_blank">
+          Open-source code
         </a>
-      </header>
+        , by Kateryna Nykonenko from
+        <a href="https://shecodes.io/" target="_blank">
+          She Codes
+        </a>
+      </div>
     </div>
   );
 }
